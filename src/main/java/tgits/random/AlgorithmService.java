602634fa773e36d594a5c08e4e665aa3dcaddece
Christophe Vaudry <christophe.vaudry@ext.ingroupe.com>
2021-12-27 09:16:43 +0100

    Ajout d'un service avec des exemples d'utilisation des constructeurs statiques pour créer des générateurs de nombres aléatoires.

--- src/main/java/tgits/random/AlgorithmService.java
+++ src/main/java/tgits/random/AlgorithmService.java
@@ -1,22 +1,35 @@
 package tgits.random;
 
+import java.util.random.RandomGenerator;
 import java.util.random.RandomGeneratorFactory;
 import java.util.stream.Collectors;
 
 public class AlgorithmService {
 
-    public String allAlgorithmsAsString() {
-        return RandomGeneratorFactory.all().map(fac -> "Algorithm name : %s - group : %s - characteristics : {%s%s%s%s%s%s%s%s stateBits: %d }".formatted(
-                fac.name(),
-                fac.group(),
-                fac.isSplittable() ? " splitable" : "",
-                fac.isStreamable() ? " streamable" : "",
-                fac.isJumpable() ? " jumpable" : "",
-                fac.isArbitrarilyJumpable() ? " arbitrary-jumpable" : "",
-                fac.isLeapable() ? " leapable" : "",
-                fac.isHardware() ? " hardware" : "",
-                fac.isStatistical() ? " statistical" : "",
-                fac.isStochastic() ? " stochastic" : "",
-                fac.stateBits())).sorted().collect(Collectors.joining("\n"));
-    }
+	public String allAlgorithmsAsString() {
+		return RandomGeneratorFactory.all().map(fac -> "Algorithm name : %s - group : %s - characteristics : {%s%s%s%s%s%s%s%s stateBits: %d }".formatted(
+				fac.name(),
+				fac.group(),
+				fac.isSplittable() ? " splittable" : "",
+				fac.isStreamable() ? " streamable" : "",
+				fac.isJumpable() ? " jumpable" : "",
+				fac.isArbitrarilyJumpable() ? " arbitrary-jumpable" : "",
+				fac.isLeapable() ? " leapable" : "",
+				fac.isHardware() ? " hardware" : "",
+				fac.isStatistical() ? " statistical" : "",
+				fac.isStochastic() ? " stochastic" : "",
+				fac.stateBits())).sorted().collect(Collectors.joining("\n"));
+	}
+
+	public RandomGenerator generatorFromAlgorithmName(String name) {
+		return switch (name) {
+			case "L128X1024MixRandom", "L128X128MixRandom", "L128X256MixRandom",
+					"L32X64MixRandom", "L64X1024MixRandom", "L64X128MixRandom",
+					"L64X128StarStarRandom", "L64X256MixRandom" -> RandomGenerator.SplittableGenerator.of(name);
+			case "Random", "SecureRandom" -> RandomGenerator.of(name);
+			case "SplittableRandom" -> RandomGenerator.StreamableGenerator.of(name);
+			case "Xoroshiro128PlusPlus", "Xoshiro256PlusPlus" -> RandomGenerator.LeapableGenerator.of(name);
+			default -> RandomGenerator.getDefault();
+		};
+	}
 }
--- src/test/java/tgits/random/AlgorithmResourceTest.java
+++ src/test/java/tgits/random/AlgorithmResourceTest.java
@@ -12,17 +12,17 @@ public class AlgorithmResourceTest {
     @Test
     public void testHelloEndpoint() {
         String expectedString = """
-                Algorithm name : L128X1024MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 1152 }
-                Algorithm name : L128X128MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 256 }
-                Algorithm name : L128X256MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 384 }
-                Algorithm name : L32X64MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 96 }
-                Algorithm name : L64X1024MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 1088 }
-                Algorithm name : L64X128MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 192 }
-                Algorithm name : L64X128StarStarRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 192 }
-                Algorithm name : L64X256MixRandom - group : LXM - characteristics : { splitable streamable statistical stateBits: 320 }
+                Algorithm name : L128X1024MixRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 1152 }
+                Algorithm name : L128X128MixRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 256 }
+                Algorithm name : L128X256MixRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 384 }
+                Algorithm name : L32X64MixRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 96 }
+                Algorithm name : L64X1024MixRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 1088 }
+                Algorithm name : L64X128MixRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 192 }
+                Algorithm name : L64X128StarStarRandom - group : LXM - characteristics : { splittable streamable statistical stateBits: 192 }
+                Algorithm name : L64X256MixRandom - group : LXM - characteristics : { splitctable streamable statistical stateBits: 320 }
                 Algorithm name : Random - group : Legacy - characteristics : { statistical stateBits: 48 }
                 Algorithm name : SecureRandom - group : Legacy - characteristics : { stochastic stateBits: 2147483647 }
-                Algorithm name : SplittableRandom - group : Legacy - characteristics : { splitable streamable statistical stateBits: 64 }
+                Algorithm name : SplittableRandom - group : Legacy - characteristics : { splittable streamable statistical stateBits: 64 }
                 Algorithm name : Xoroshiro128PlusPlus - group : Xoroshiro - characteristics : { streamable jumpable leapable statistical stateBits: 128 }
                 Algorithm name : Xoshiro256PlusPlus - group : Xoshiro - characteristics : { streamable jumpable leapable statistical stateBits: 256 }""";
         given()
