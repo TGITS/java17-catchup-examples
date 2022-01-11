2deed06474edbf64c84680e71a772bdb70069897
Christophe Vaudry <christophe.vaudry@ext.ingroupe.com>
2022-01-11 16:05:55 +0100

    feat(): article java 17
    Travail sur les pièces d'échiquier

--- src/main/java/tgits/sealed/chess/Piece.java
+++ src/main/java/tgits/sealed/chess/Piece.java
@@ -1,49 +1,45 @@
 package tgits.sealed.chess;
 
-public abstract sealed class Piece permits Pawn, Bishop, Knight, Rook, Queen, King {
-
-    protected Color color;
-
-    @Override
-    public abstract String toString();
+public sealed interface Piece permits Pawn, Bishop, Knight, Rook, Queen, King {
+    Color color();
 }
 
-final class Bishop extends Piece {
+record Bishop(Color color) implements Piece {
     @Override
     public String toString() {
         return "%s Bishop".formatted(this.color.toString());
     }
 }
 
-final class King extends Piece {
+record King(Color color) implements Piece {
     @Override
     public String toString() {
         return "%s King".formatted(this.color.toString());
     }
 }
 
-final class Knight extends Piece {
+record Knight(Color color) implements Piece {
     @Override
     public String toString() {
         return "%s Knight".formatted(this.color.toString());
     }
 }
 
-final class Pawn extends Piece {
+record Pawn(Color color) implements Piece {
     @Override
     public String toString() {
         return "%s Pawn".formatted(this.color.toString());
     }
 }
 
-final class Queen extends Piece {
+record Queen(Color color) implements Piece {
     @Override
     public String toString() {
         return "%s Queen".formatted(this.color.toString());
     }
 }
 
-final class Rook extends Piece {
+record Rook(Color color) implements Piece {
     @Override
     public String toString() {
         return "%s Rook".formatted(this.color.toString());
