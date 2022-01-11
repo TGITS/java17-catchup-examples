8b3a5abb9c0f9de052bb57294454718790bd54d9
Christophe Vaudry <christophe.vaudry@ext.ingroupe.com>
2022-01-11 08:41:04 +0100

    feat(): article sur Java 17
    Travail sur la partie nombres aléatoires

--- src/main/java/tgits/random/dice/RandomDeviceResource.java
+++ src/main/java/tgits/random/dice/RandomDeviceResource.java
@@ -1,6 +1,11 @@
 package tgits.random.dice;
 
+import io.smallrye.mutiny.Multi;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.random.RandomGenerator;
 
 @Path("/java17/random")
 public class RandomDeviceResource {
@@ -9,5 +14,29 @@ public class RandomDeviceResource {
 
     public RandomDeviceResource(RandomDeviceService randomDeviceService){
         this.randomDeviceService = randomDeviceService;
+    }
+
+    @GET
+    @Path("/flip/coin")
+    public boolean flipCoin() {
+        return this.randomDeviceService.flipCoin();
+    }
+
+    @GET
+    @Path("/roll/{dice_type}")
+    public int rollDice(@PathParam("dice_type") String diceType) {
+        return this.randomDeviceService.rollDice(diceType);
+    }
+
+    @GET
+    @Path("/stream/double/{number}")
+    public Multi<Double> streamOfRandomDoubles(@PathParam("number") int number) {
+        return Multi.createFrom().items(() -> RandomGenerator.getDefault().doubles(number).boxed());
+    }
+
+    @GET
+    @Path("/stream/integer/{number}")
+    public Multi<Integer> streamOfRandomIntegers(@PathParam("number") int number) {
+        return Multi.createFrom().items(() -> RandomGenerator.getDefault().ints(number).boxed());
     }
 }
