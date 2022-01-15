6d2f5efa5cfd8571855342e3230c9f7adece959b
cvaudry <cvaudry@norsys.fr>
2022-01-15 17:07:01 +0100

    feat(): article java 17
    Correction formelle

--- src/main/java/tgits/record/tuple/Pair.java
+++ src/main/java/tgits/record/tuple/Pair.java
@@ -1,7 +1,6 @@
 package tgits.record.tuple;
 
 public record Pair<U,V>(U first, V second) {
-
     public static <U,V>Pair<U,V> of(U first, V second) {
         return new Pair<>(first,second);
     }
